0ad3371d7a46c2a72d75a23800063cef289b2e2c
Luiz Augusto <luis2848@gmail.com>
2024-02-04 21:51:07 -0300

    Adicionando valores as contas

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,67 @@
 <script setup>
-import { ref, onMounted, computed, watch } from "vue";
+  import { ref, onMounted, computed, watch } from "vue";
 
-const bills = ref([]);
-const name = ref("");
+  const bills = ref([]);
+  const name = ref("");
 
-const input_content = ref("");
-const input_category = ref(null);
+  const input_content = ref("");
+  const input_price = ref("")
+  const input_category = ref(null);
 
-const bills_asc = computed(() =>
-  bills.value.sort((a, b) => {
-    return b.createdAt - a.createdAt;
+  const bills_asc = computed(() =>
+    bills.value.sort((a, b) => {
+      return b.createdAt - a.createdAt;
+    })
+  );
+
+  const total_price = computed(()=>{
+   return bills.value.reduce((sum, bill) => {
+      return sum + bill.price;
+    }, 0)
   })
-);
 
 
-watch(
-  bills,
-  (newVal) => {
-    localStorage.setItem("bills", JSON.stringify(newVal));
-  },
-  { deep: true }
-);
+  watch(
+    bills,
+    (newVal) => {
+      localStorage.setItem("bills", JSON.stringify(newVal));
+    },
+    { deep: true }
+  );
 
-watch(name, (newVal) => {
-  localStorage.setItem("name", newVal);
-});
-
-
-
-
-const addBill = () => {
-  if (input_content.value.trim === '' || input_category.value === null) {
-    return;
-  }
-
-  bills.value.push({
-    content: input_content.value,
-    category: input_category.value,
-    done: false,
-    editable: false,
-    createdAt: new Date().getTime()
+  watch(name, (newVal) => {
+    localStorage.setItem("name", newVal);
   });
 
-};
-
-const removeBill = bill => {
-    bills.value = bills.value.filter(t => t !== bill)
-}
 
 
 
-onMounted(() => {
-  name.value = localStorage.getItem('name') || '';
-  bills.value = JSON.parse(localStorage.getItem('bills')) || [];
-});
+  const addBill = () => {
+    if (input_content.value.trim === '' || input_category.value === null) {
+      return;
+    }
+
+    bills.value.push({
+      content: input_content.value,
+      price: input_price.value,
+      category: input_category.value,
+      done: false,
+      editable: false,
+      createdAt: new Date().getTime()
+    });
+
+  };
+
+  const removeBill = bill => {
+      bills.value = bills.value.filter(t => t !== bill)
+  }
+
+
+
+  onMounted(() => {
+    name.value = localStorage.getItem('name') || '';
+    bills.value = JSON.parse(localStorage.getItem('bills')) || [];
+  });
 </script>
 
 <template>
@@ -73,6 +81,14 @@ onMounted(() => {
           placeholder="Exemplo: conta de luz"
           v-model="input_content"
         />
+
+        <h4>Quail o valor?</h4>
+        <input
+          type="number"
+          placeholder="Exemplo: 100"
+          v-model="input_price"
+        />
+
         <h4>A conta é sua ou da casa?</h4>
 
         <div class="options">
@@ -120,14 +136,20 @@ onMounted(() => {
 
             <div class="bill-content">
                 <input type="text" v-model="bill.content">
+                <div>
+                R$ <input type="number" v-model="bill.price" class="bill-price">
+                </div>
             </div>
+
 
             <div class="actions">
                 <button class="delete" @click="removeBill(bill)">Delete</button>
             </div>
-    
+
+            
         </div>
       </div>
     </section>
+      Valor total : R${{ total_price }}
   </main>
 </template>
